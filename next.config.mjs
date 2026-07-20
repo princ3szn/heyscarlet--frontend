2f5978e091012b2a434da7fedd20f21df19ed950
princ3szn <aminufumenprince@gmail.com>
2026-07-20 20:34:26 +0100

    fix: backend api logic

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/backend/:path*',
+        destination: 'https://api.heyscarlet.ai/:path*',
+      },
+    ]
+  },
+};
 
 export default nextConfig;
